619c83377e09757cfc3329ec5f7c4ffe4c26d05e
sabsger <sabsger@ya.ru>
2022-05-02 19:25:01 +0500

    Модуль авторизации

--- clicker/clicker/settings.py
+++ clicker/clicker/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'clicker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
